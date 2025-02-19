1ced102b59c1f4524122ad5b4e3f44ecb40c7830
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-02-19 22:23:20 +0200

    update

---
+++ lessons/lesson-14/js/lesson-14.js
@@ -0,0 +1,161 @@
+const btnBegin = document.querySelector('.btn-begin')
+
+const lesson14 = () => {
+	//todo [0 - Робота з масивом об’єктів]
+	console.log(
+		'%c [0 - Робота з масивом об’єктів] ',
+		'color: yellow; background-color: #2274A5'
+	)
+	//! Для виконання всіх завдань вкористовуй цей масив об'єктів:
+	const users = [
+		{
+			id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+			name: 'Moore Hensley',
+			email: 'moorehensley@indexia.com',
+			eyeColor: 'blue',
+			friends: ['Sharron Pace'],
+			isActive: false,
+			balance: 2811,
+			skills: ['ipsum', 'lorem'],
+			gender: 'male',
+			age: 37
+		},
+		{
+			id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+			name: 'Sharlene Bush',
+			email: 'sharlenebush@tubesys.com',
+			eyeColor: 'blue',
+			friends: ['Briana Decker', 'Sharron Pace'],
+			isActive: true,
+			balance: 3821,
+			skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+			gender: 'female',
+			age: 34
+		},
+		{
+			id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+			name: 'Ross Vazquez',
+			email: 'rossvazquez@xinware.com',
+			eyeColor: 'green',
+			friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+			isActive: false,
+			balance: 3793,
+			skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+			gender: 'male',
+			age: 24
+		},
+		{
+			id: '249b6175-5c30-44c6-b154-f120923736f5',
+			name: 'Elma Head',
+			email: 'elmahead@omatom.com',
+			eyeColor: 'green',
+			friends: ['Goldie Gentry', 'Aisha Tran'],
+			isActive: true,
+			balance: 2278,
+			skills: ['adipisicing', 'irure', 'velit'],
+			gender: 'female',
+			age: 21
+		},
+		{
+			id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+			name: 'Carey Barr',
+			email: 'careybarr@nurali.com',
+			eyeColor: 'blue',
+			friends: ['Jordan Sampson', 'Eddie Strong'],
+			isActive: true,
+			balance: 3951,
+			skills: ['ex', 'culpa', 'nostrud'],
+			gender: 'male',
+			age: 27
+		},
+		{
+			id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+			name: 'Blackburn Dotson',
+			email: 'blackburndotson@furnigeer.com',
+			eyeColor: 'brown',
+			friends: ['Jacklyn Lucas', 'Linda Chapman'],
+			isActive: false,
+			balance: 1498,
+			skills: ['non', 'amet', 'ipsum'],
+			gender: 'male',
+			age: 38
+		},
+		{
+			id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+			name: 'Sheree Anthony',
+			email: 'shereeanthony@kog.com',
+			eyeColor: 'brown',
+			friends: ['Goldie Gentry', 'Briana Decker'],
+			isActive: true,
+			balance: 2764,
+			skills: ['lorem', 'veniam', 'culpa'],
+			gender: 'female',
+			age: 39
+		}
+	]
+	console.log('users:', users)
+	console.log('--------------------------------------------------')
+
+	//todo [1]
+	console.log('%c [1] ', 'color: yellow; background-color: #2274A5')
+	//? Отримай масив імен всіх користувачів (поле name).
+	const getUserNames = users => {
+		//! твій код
+		const allUsersByName = users.map(user => user.name)
+		return allUsersByName
+	}
+
+	console.log(getUserNames(users)) //! [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+	console.log('--------------------------------------------------')
+
+	//todo [2]
+	console.log('%c [2] ', 'color: yellow; background-color: #2274A5')
+	//? Отримай масив об'єктів користувачів за кольором очей (поле eyeColor).
+	const getUsersWithEyeColor = (users, color) => {
+		//! твій код
+		const allUsersWithEyeColor = users.filter(user => user.eyeColor === color)
+		return allUsersWithEyeColor
+	}
+
+	console.log(getUsersWithEyeColor(users, 'blue')) //! [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+	console.log('--------------------------------------------------')
+
+	//todo [3]
+	console.log('%c [3] ', 'color: yellow; background-color: #2274A5')
+	//? Отримати масив імен користувачів за статтю (поле gender).
+	const getUsersWithGender = (users, gender) => {
+		//! твій код
+		const allUsersWithGender = users.filter(user => user.gender === gender)
+		return allUsersWithGender
+	}
+
+	console.log(getUsersWithGender(users, 'male')) //! [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+	console.log('--------------------------------------------------')
+
+	//todo [4]
+	console.log('%c [4] ', 'color: yellow; background-color: #2274A5')
+	//? Отримати масив тільки неактивних користувачів (поле isActive).
+	const getInactiveUsers = users => {
+		//! твій код
+		const allInactiveUsers = users.filter(user => !user.isActive)
+		return allInactiveUsers
+	}
+
+	console.log(getInactiveUsers(users)) //! [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
+	console.log('--------------------------------------------------')
+
+	//todo [5]
+	console.log('%c [5] ', 'color: yellow; background-color: #2274A5')
+	//? Отримати користувача (не масив) по email (поле email, він унікальний).
+	const getUserWithEmail = (users, email) => {
+		//! твій код
+		const userByEmail = users.find(user => user.email === email)
+		return userByEmail
+	}
+
+	console.log(getUserWithEmail(users, 'shereeanthony@kog.com')) //! {об'єкт користувача Sheree Anthony}
+	console.log(getUserWithEmail(users, 'elmahead@omatom.com')) //! {об'єкт користувача Elma Head}
+	console.log('--------------------------------------------------')
+}
+
+btnBegin.addEventListener('click', lesson14)
