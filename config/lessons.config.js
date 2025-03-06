a71a45efceeb7c7407f01587b0214a5277cc049c
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-03-06 20:34:38 +0200

    update

---
+++ config/lessons.config.js
@@ -0,0 +1,122 @@
+export const LessonsList = [
+	{
+		id: 1,
+		title: 'Знайомство з JS. Змінні. Типи даних. Взаємодія з користувачем',
+		path: './lessons/lesson-01/lesson-1.html'
+	},
+	{
+		id: 2,
+		title: 'Основні оператори. Числа',
+		path: './lessons/lesson-02/lesson-2.html'
+	},
+	{
+		id: 3,
+		title: 'Рядки',
+		path: './lessons/lesson-03/lesson-3.html'
+	},
+	{
+		id: 4,
+		title: 'Логічні оператори. Розгалуження.',
+		path: './lessons/lesson-04/lesson-4.html'
+	},
+	{
+		id: 5,
+		title: 'Розгалуження. Інструкція switch. Область видимості. Практика',
+		path: './lessons/lesson-05/lesson-5.html'
+	},
+	{
+		id: 6,
+		title: 'Цикли',
+		path: './lessons/lesson-06/lesson-6.html'
+	},
+	{
+		id: 7,
+		title: 'Масиви. Ітерація',
+		path: './lessons/lesson-07/lesson-7.html'
+	},
+	{
+		id: 8,
+		title: 'Методи масиву',
+		path: './lessons/lesson-08/lesson-8.html'
+	},
+	{
+		id: 9,
+		title: 'Функції',
+		path: './lessons/lesson-09/lesson-9.html'
+	},
+	{
+		id: 10,
+		title: 'Колбек і стрілочні функції.',
+		path: './lessons/lesson-10/lesson-10.html'
+	},
+	{
+		id: 11,
+		title: 'Об’єкти. Створення, властивості, методи об’єкту',
+		path: './lessons/lesson-11/lesson-11.html'
+	},
+	{
+		id: 12,
+		title: 'Об’єкти. Перебір об’єкту',
+		path: './lessons/lesson-12/lesson-12.html'
+	},
+	{
+		id: 13,
+		title: 'Об’єкти. Деструкткризація об’єктів',
+		path: './lessons/lesson-13/lesson-13.html'
+	},
+	{
+		id: 14,
+		title: 'Імперативний і декларативний код. Чисті функції',
+		path: './lessons/lesson-14/lesson-14.html'
+	},
+	{
+		id: 15,
+		title: 'Перебираючі методи масиву',
+		path: './lessons/lesson-15/lesson-15.html'
+	},
+	{
+		id: 16,
+		title: 'ООП',
+		path: './lessons/lesson-16/lesson-16.html'
+	},
+	{
+		id: 17,
+		title: 'DOM',
+		path: './lessons/lesson-17/lesson-17.html'
+	},
+	{
+		id: 18,
+		title: 'DOM (Частина 2)',
+		path: './lessons/lesson-18/lesson-18.html'
+	},
+	{
+		id: 19,
+		title: 'Події',
+		path: './lessons/lesson-19/lesson-19.html'
+	},
+	{
+		id: 20,
+		title: 'Події. Події клавіатури та миші',
+		path: './lessons/lesson-20/lesson-20.html'
+	},
+	{
+		id: 21,
+		title: 'Командний проєкт (1)',
+		path: './lessons/lesson-21/lesson-21.html'
+	},
+	{
+		id: 22,
+		title: 'Командний проєкт (2)',
+		path: './lessons/lesson-22/lesson-22.html'
+	},
+	{
+		id: 23,
+		title: 'Консультація (відповіді на питання)',
+		path: './lessons/lesson-23/lesson-23.html'
+	},
+	{
+		id: 24,
+		title: 'Захист проєкту',
+		path: './lessons/lesson-24/lesson-24.html'
+	}
+]
--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+import { LessonsList } from './config/lessons.config.js'
+
 function main() {
+	const lessonsList = document.querySelector('#lessons-list')
+
+	lessonsList.innerHTML = LessonsList.map(lesson => {
+		return `<li>
+			<a href="${lesson.path}" class="lesson-link"
+				>${lesson.id}. ${lesson.title}
+			</a>
+		</li>`
+	}).join('')
+
 	console.log('App work')
 }
 
